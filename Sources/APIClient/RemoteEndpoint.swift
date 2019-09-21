7efe2da97c88c046788f1c19edd51e9813cfac36
Scott Petit <scott@hecho.io>
2019-09-21 10:57:46 -0500

    Adds ability to specify JSONDecoder on RemoteEndpoint

--- Sources/APIClient/RemoteEndpoint.swift
+++ Sources/APIClient/RemoteEndpoint.swift
@@ -10,25 +10,33 @@ public struct RemoteEndpoint<A: Decodable> {
     public let headers: [String: String]
     public let parameters: [String: String]?
     public let acceptableStatusCode: (Int) -> Bool
+    public let decoder: JSONDecoder?
     public let parse: (Data) -> Result<A, DecodingError>
 
-    public init(path: PathConvertible, method: HTTPMethod, headers: [String: String] = [:], parameters: [String: String]? = nil, acceptableStatusCode: @escaping (Int) -> Bool = expected200to300(_:), parse: @escaping (Data) -> Result<A, DecodingError> = decode) {
+    public init(path: PathConvertible, method: HTTPMethod, headers: [String: String] = [:], parameters: [String: String]? = nil, acceptableStatusCode: @escaping (Int) -> Bool = expected200to300(_:), decoder: JSONDecoder? = nil, parse: @escaping (Data) -> Result<A, DecodingError> = decode) {
         self.path = path
         self.method = method
         self.headers = headers
         self.parameters = parameters
         self.acceptableStatusCode = acceptableStatusCode
-        self.parse = parse
+        self.decoder = decoder
+        if let _decoder = decoder {
+            self.parse = { data in
+                decode(data, with: _decoder)
+            }
+        } else {
+            self.parse = parse
+        }
     }
 
     public func append(_ headers: [String: String], uniquingKeysWith combine: ((String, String) -> String)? = nil) -> RemoteEndpoint<A> {
         let newHeaders = self.headers.merging(headers) { (old, new) -> String in
             return combine?(old, new) ?? new
         }
-        return RemoteEndpoint(path: self.path, method: self.method, headers: newHeaders, parameters: self.parameters, parse: parse)
+        return RemoteEndpoint(path: self.path, method: self.method, headers: newHeaders, parameters: self.parameters, decoder: self.decoder, parse: self.parse)
     }
 
     public func map<B: Decodable>(_ transform: @escaping (A) -> B) -> RemoteEndpoint<B> {
-        return RemoteEndpoint<B>(path: self.path, method: self.method, headers: self.headers, parameters: self.parameters) { self.parse($0).map(transform) }
+        return RemoteEndpoint<B>(path: self.path, method: self.method, headers: self.headers, parameters: self.parameters, decoder: self.decoder) { self.parse($0).map(transform) }
     }
 }
